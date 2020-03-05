f87feec4487d3f3e081085ee9c1ed603d370aa29
Amsh285 <58710888+Amsh285@users.noreply.github.com>
2020-03-05 22:57:48 +0100

    Implemented Reading of shareentries
    - Todo: proper exceptionhandling
    - Todo: improvement of Filepath handling

--- Utilities.h
+++ Utilities.h
@@ -1,6 +1,9 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
+#include "CsvLineException.h"
+#include "ShareEntry.h"
+
 #include <vector>
 #include <string>
 
@@ -15,6 +18,11 @@ namespace stdstring
     std::string ToLower(const std::string& value);
     bool Equals(const std::string& left, const std::string& right, const StringComparisonOptions option);
     std::string join(const std::vector<std::string> value, const std::string& separator);
+}
+
+namespace shareentryfactory
+{
+    ShareEntry *CreateFromCsvString(const std::string& value);
 }
 
 #endif // UTILITIES_H
