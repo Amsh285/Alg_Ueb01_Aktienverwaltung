888d809bee91e8c0bdab221d108213fc7bc39f9b
Amsh285 <58710888+Amsh285@users.noreply.github.com>
2020-03-09 10:49:08 +0100

    Weiterentwicklung Aktien

--- ShareContainer.cpp
+++ ShareContainer.cpp
@@ -1,11 +1,21 @@
 #include "ShareContainer.h"
 
-ShareContainer::ShareContainer()
+ShareContainer::ShareContainer(Share *value)
 {
-    //ctor
+    if(value == nullptr)
+        throw std::invalid_argument("value cannot null.");
+
+    this->value = value;
+    this->hasValue = true;
 }
 
 ShareContainer::~ShareContainer()
 {
     //dtor
+}
+
+void ShareContainer::DeleteContent()
+{
+    delete this->value;
+    this->hasValue = false;
 }
